0ab46b88095ae8cd586f2b8d5d98b5c4a909c0f7
todatoday <taki.souad-90@hotmail.com>
2024-11-27 19:45:21 +0100

    sidbar-dynamic

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,50 +1,17 @@
-const sidebar = document.querySelectorAll(".animal-name"); 
-let spanbuttn = document.querySelector("span");
-
-sidebar.forEach(sidebar => {
-    sidebar.addEventListener("click", () => {
-        document.querySelector(".active")?.classList.remove("active");
-        sidebar.classList.add("active");  
-    })
-    
-})
- 
-
 const togglebuttn = document.querySelector(".menu-btn");
 togglebuttn.addEventListener("click", () =>{
    // document.querySelector("span").classList.toggle("open")
-    document.querySelector(".side-content").classList.toggle("open")
+    document.querySelector(".asid-home-links").classList.toggle("open")
 })
 
+const links = ["Echidna", "Tasmanian Devil ", "quokka", "frill-necked lizard", "Hawksbill Turtle", "Perentie ", "Cassowary", "Kookaburra", "Yellow Tailed Black Cockatoo" ];
+ 
+let sidebarLinks = document.querySelector(".sidebar-linkss");
 
+for(let i = 0; i < links.length; i++) {
+    let tagLink = document.createElement("a");
+    tagLink.textContent = links[i]
+    tagLink.classList.add("links") 
+    sidebarLinks.appendChild(tagLink)
 
-
-
-
-// let animales = [mammals, reptiles, birds];
-
-// let mammals = {
-//     name : "Echidna",
-//     lifespan : "50 years",
-//     group : "mammals",
-//     food : "insects such as ants and termites, beetle larvae and worms",
-//     description : "    description: Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.",
-//     length : "76cm",
-//     weight : "10kg",
-//     found: "Throughout Australia"
-// }
-
-// function animale (name, lifespan, group, food, description, length, weight,found) {
-//     this.name = name;
-//     this.lifespan = lifespan;
-//     this.group = group;
-//     this.food = food;
-//     this.description = description;
-//     this.length = length;
-//     this.weight = weight;
-//     this.found = found;
-// }
-
-
-
-
+}
