efe40a26d9a918d54af64a60310fadb93044686b
ranjithavelusamy <ranjithavelusamy84@gmail.com>
2024-12-02 14:41:16 +0100

    headernew

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,35 +1,10 @@
-const togglButtn = document.querySelector(".menu-btn");
-togglButtn.addEventListener("click", () =>{
+//const togglButtn = document.querySelector(".menu-btn");
+//togglButtn.addEventListener("click", () =>{
    // document.querySelector("span").classList.toggle("open")
-    document.querySelector(".asid-home-links").classList.toggle("open")
-})
-
-const links = ["Echidna", "Tasmanian Devil ", "quokka", "frill-necked lizard", "Hawksbill Turtle", "Perentie ", "Cassowary", "Kookaburra", "Yellow Tailed Black Cockatoo" ];
- 
-let sidebarLinks = document.querySelector(".sidebar-linkss");
-
-for(let i = 0; i < links.length; i++) {
-    let tagLink = document.createElement("a");
-    tagLink.textContent = links[i]
-    tagLink.classList.add("links") 
-    sidebarLinks.appendChild(tagLink)
-
-}
+ //   document.querySelector(".asid-home-links").classList.toggle("open")
+//})
 
 
-
-const sidebar = document.querySelectorAll(".sidebar-item"); 
-let spanbuttn = document.querySelector("span");
-
-sidebar.forEach(sidebar => {
-    sidebar.addEventListener("click", () => {
-        document.querySelector(".active")?.classList.remove("active");
-        sidebar.classList.add("active");  
-    })
-    
-})
- 
-<<<<<<< HEAD
 
 //*const togglebuttn = document.querySelector(".menu-btn");
 //*togglebuttn.addEventListener("click", () =>{
@@ -69,6 +44,3 @@ sidebar.forEach(sidebar => {
 
 
 
-=======
- 
->>>>>>> main
